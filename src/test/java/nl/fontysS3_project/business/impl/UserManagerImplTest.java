9a81f03406ceea65f04db508fba606209efaf781
slimjanpot <slimjanpot@gmail.com>
2023-12-01 21:33:10 +0100

    Fixed the testing

--- src/test/java/nl/fontysS3_project/business/impl/UserManagerImplTest.java
+++ src/test/java/nl/fontysS3_project/business/impl/UserManagerImplTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,8 +22,12 @@ import static org.mockito.Mockito.*;
 class UserManagerImplTest {
     @Mock
     private UserRepository userRepo;
+    @Mock
+    private PasswordEncoder passwordEncoder;
     @InjectMocks
     private UserManagerImpl userManager;
+
+
 
     private final UserEntity userentity = UserEntity.builder()
                 .id(1)
@@ -84,21 +89,46 @@ class UserManagerImplTest {
 
     @Test
     void createUser() {
-        when(userRepo.save(any(UserEntity.class))).thenReturn(userentity);
+        when(userRepo.save(any(UserEntity.class))).thenAnswer(invocation -> {
+            UserEntity savedUserEntity = invocation.getArgument(0);
+            // Assert the properties of the saved user entity
+            assertEquals("Steven", savedUserEntity.getName());
+            assertEquals("Steve", savedUserEntity.getUsername());
+            assertEquals("email", savedUserEntity.getEmail());
+            assertEquals(23, savedUserEntity.getAge());
+            assertEquals("Somewhere", savedUserEntity.getLocation());
+            assertEquals("I love this", savedUserEntity.getBio());
+            // Assert the encoded password
+            assertTrue(passwordEncoder.matches("pass", savedUserEntity.getPassword()));
+            return savedUserEntity;
+        });
 
+        when(passwordEncoder.encode("pass")).thenReturn("encodedPass");
+
+        // Mocking the matches operation for password verification
+        when(passwordEncoder.matches("pass", "encodedPass")).thenReturn(true);
+
+
+        // Calling the method under test
         User savedUser = userManager.createUser(user1);
 
-        verify(userRepo).save(userentity);
+        // Verifying that the save method was called with the correct arguments
+        verify(userRepo).save(any(UserEntity.class));
+
+        verify(passwordEncoder).encode("pass");
+
+        // Verifying that the matches method was called with the correct arguments
+        verify(passwordEncoder).matches("pass", "encodedPass");
 
         assertNotNull(savedUser);
-
         assertEquals("Steven", savedUser.getName());
         assertEquals("Steve", savedUser.getUsername());
         assertEquals("email", savedUser.getEmail());
         assertEquals(23, savedUser.getAge());
         assertEquals("Somewhere", savedUser.getLocation());
         assertEquals("I love this", savedUser.getBio());
-        assertEquals("pass", savedUser.getPassword());
+        // Adjust the assertion based on your actual password verification logic
+        assertEquals("encodedPass", savedUser.getPassword());
     }
 
     @Test
